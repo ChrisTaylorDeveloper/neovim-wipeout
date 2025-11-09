864bbe630ed53f53cfbc633e84a348317babe0ae
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-11-09 13:56:11 +0700

    Support my dual lazy / kickstart config.

--- DELETE-DANGER.sh
+++ DELETE-DANGER.sh
@@ -4,5 +4,13 @@ rm -rf ~/.cache/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
 
-rm -f ~/.config/nvim/lazy-lock.json
-rm -f ~/.config/nvim/lazyvim.json
+rm -f ~/.config/nvimlazy/lazy-lock.json
+rm -f ~/.config/nvimlazy/lazyvim.json
+
+rm -rf ~/.cache/nvimlazy
+rm -rf ~/.local/share/nvimlazy
+rm -rf ~/.local/state/nvimlazy
+
+rm -rf ~/.cache/nvimkick
+rm -rf ~/.local/share/nvimkick
+rm -rf ~/.local/state/nvimkick
