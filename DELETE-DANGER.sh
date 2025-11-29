9a7ac11f897162dbc682ad0f723d9fb98a2ffcdc
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-11-29 13:48:30 +0700

    Revert to removing generic nvim state, not kickstart & lazy variants.

--- DELETE-DANGER.sh
+++ DELETE-DANGER.sh
@@ -4,13 +4,5 @@ rm -rf ~/.cache/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
 
-rm -f ~/.config/nvimlazy/lazy-lock.json
-rm -f ~/.config/nvimlazy/lazyvim.json
-
-rm -rf ~/.cache/nvimlazy
-rm -rf ~/.local/share/nvimlazy
-rm -rf ~/.local/state/nvimlazy
-
-rm -rf ~/.cache/nvimkick
-rm -rf ~/.local/share/nvimkick
-rm -rf ~/.local/state/nvimkick
+rm -f ~/.config/nvim/lazy-lock.json
+rm -f ~/.config/nvim/lazyvim.json
